aeabe8d9fb051bdd60538ae81ef09ee6dfb26e30
tukhoza <39373673+tukhoza@users.noreply.github.com>
2023-06-02 22:20:40 +0100

    Create App.java

---
+++ Java intro/src/app/App.java
@@ -0,0 +1,36 @@
+package app;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The java introduction for Tumelo\n");
+		System.out.println("Java primatve");
+		
+		byte thePrimateByte = 62;
+		System.out.println("The primative byte : " + thePrimateByte);
+		
+		int thePrimativeInt = 1;
+		
+		System.out.println("The primative int  : " + thePrimativeInt);
+		
+		float thePrimitiveFloat = 1f;
+		
+		System.out.println("The primative float : " + thePrimitiveFloat);
+		
+		char thePrimative = 'A';
+		
+		System.out.println("The primative char : " + thePrimative);
+		
+		double thePrimativeDouble = 1;
+		
+		System.out.println("The promative double : " + thePrimativeDouble);
+		
+		long ThePrimativeLong = 1L;
+		
+		System.out.println("The primative long : " + ThePrimativeLong);
+		
+
+	}
+
+}
